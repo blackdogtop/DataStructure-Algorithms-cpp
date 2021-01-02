8d181bee11dd508c4f4529dbf92eb05409b65c23
blackdogtop <840474551@qq.com>
2021-01-02 10:11:16 +0800

    update code

--- LinkedList/isPalindrome.cpp
+++ LinkedList/isPalindrome.cpp
@@ -24,26 +24,18 @@ public:
     bool isPalindrome(ListNode* head) {
         /* 递归
          * Complexity:
-         *      time: ?
-         *      space: O(N/2)
+         *      time: O(N)
+         *      space: O(N)
          *      */
         if (!head || !head->next){return true;}
-        bool res = recursion(head, nullptr);
-        return res;
-    }
 
-    bool recursion(ListNode* head, ListNode* last){
-        ListNode* end = head;
-        while(end->next != last){
-            end = end->next;
-        }
-        if(head->next == end || head->next->next == end){
-            if(head->val != end->val){return false;}
-            return true;
-        }
-        if (head->val != end->val){return false;}
-        bool subRec = recursion(head->next, end);
-        if(!subRec){return false;}
+        ListNode* lastSec = head;
+        while(lastSec->next->next){lastSec = lastSec->next;}  // 倒数第二节点
+        ListNode* last = lastSec->next;
+        lastSec->next = nullptr;
+
+        if (head->val != last->val){return false;}
+        if (! isPalindrome(head->next)){return false;}
         return true;
     }
 
@@ -96,13 +88,46 @@ public:
         }
         return true;
     }
+
+    bool isPalindromeReverse(ListNode* head) {
+        /* 中断+反转+判断
+         * Complexity:
+         *      time: O(N)
+         *      space: O(1) */
+        if (!head || !head->next){return true;}
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* right = slow->next;
+        slow->next = nullptr;
+
+        ListNode* pre = nullptr;
+        while (right){
+            ListNode* nex = right->next;
+            right->next = pre;
+            pre = right;
+            right = nex;
+        }
+        right = pre;
+
+        while (right && head){
+            if (right->val != head->val){return false;}
+            right = right->next;
+            head = head->next;
+        }
+        return true;
+    }
 };
 
 
 int main(){
     ListNode* node1 = new ListNode(1);
     ListNode* node2 = new ListNode(2);
-    ListNode* node3 = new ListNode(2);
+    ListNode* node3 = new ListNode(1);
     ListNode* node4 = new ListNode(1);
 
     node1->next = node2;
@@ -113,6 +138,7 @@ int main(){
     bool res;
 //    res = s.isPalindrome(node1);  // 递归
 //    res = s.isPalindromeStack(node1); // 栈
-    res = s.isPalindromeTwoptr(node1);  // 双指针
+//    res = s.isPalindromeTwoptr(node1);  // 双指针
+    res = s.isPalindromeReverse(node1);  // 中断+反转+判断
     cout << res << endl;
 }
